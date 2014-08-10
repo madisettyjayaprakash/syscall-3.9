d3eb330d1eb29e43fb2285f8bf4897b1c90a29f4
Morgan Phillips <morgan@artistgrowth.com>
2014-08-10 13:32:29 -0500

    Fixes bad tab alignment.

--- callcount_example.c
+++ callcount_example.c
@@ -2,7 +2,7 @@
 
 unsigned long i = 0;
 int main() {
-    syscall(314, &i);	
-	printf("You've called me %lu times.\n", i);
-	return 0;
+    syscall(314, &i);
+    printf("You've called me %lu times.\n", i);
+    return 0;
 }
